8d988c3e7467e29ac8d936fa5e95f67e9d4ee66b
Nandhakumar S <kumarnandha8983@gmail.com>
2024-02-21 21:10:36 +0530

    Modified code

--- index.js
+++ index.js
@@ -1,35 +1,33 @@
-let AddButton = document.getElementById("AddBook");
-let AddNewBooks = document.querySelector(".PopupOverlay");
-let AddButton1 = document.getElementById("adddbutton");
-let CancelButton1 = document.getElementById("cancelbutton");
-let BookName1 = document.getElementById("bookhead");
-let AuthorName1 = document.getElementById("bookauthor");
-let Description1 = document.getElementById("bookdesc");
-let TotalContainer = document.querySelector(".container");
-let NewContainer = document.getElementById("box-container");
+let popupbutton = document.getElementById("add-popup-button");
+let popupbackground = document.querySelector(".popupoverlay");
+let popupwindow = document.querySelector(".popup");
+let cancelbutton = document.getElementById("cancelpopup");
 
-AddButton.addEventListener("click",()=>{
-    AddNewBooks.style.display = "flex";
-}
-);
-
-CancelButton1.addEventListener("click",(event)=>{
-    event.preventDefault();
-    AddNewBooks.style.display = "none";
+popupbutton.addEventListener("click",()=>{
+    popupbackground.style.display = "block";
+    popupwindow.style.display = "block";
 });
 
-AddButton1.addEventListener("click",(event)=>{
+cancelbutton.addEventListener("click",(event)=>{
     event.preventDefault();
-    let a23 = BookName1.value;
-    let sade = AuthorName1.value;
-    let safsa = Description1.value;
-    console.log(a23,sade,safsa);
-    let div = document.createElement('div');
-    div.setAttribute('class','box-container');
-    div.innerHTML = `<h2>${BookName1.value}</h2>
-    <h4>${AuthorName1.value}</h4>
-    <p>${Description1.value}</p>`
-    TotalContainer.append(div);
-    console.log(div);
-    AddNewBooks.style.display = "none";
+    popupbackground.style.display = "none";
+    popupwindow.style.display = "none";
+});
+
+let addnewbook = document.getElementById("addbook");
+let BookTitleInput = document.getElementById("book-title-input");
+let AuthorNameInput = document.getElementById("author-name-input");
+let DescriptionInput = document.getElementById("description-input");
+let containers = document.querySelector(".container");
+
+addnewbook.addEventListener("click",(event)=>{
+    event.preventDefault();
+    let div = document.createElement("div");
+    div.classList.add("book-container");
+    div.innerHTML = `<h2>${BookTitleInput.value}</h2>
+    <h5>${AuthorNameInput.value}</h5>
+    <p>${DescriptionInput.value}</p>`
+    containers.append(div);
+    popupbackground.style.display = "none";
+    popupwindow.style.display = "none";
 });
